5f33a6547d848a69923a152f6fdaeb5e79cf6406
Manoj <napestershine@gmail.com>
2016-03-24 00:21:48 +0530

    fixed Login and registration with ajax and FUB

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -2,16 +2,43 @@
 namespace UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $formFactory = $this->get('fos_user.registration.form.factory');
         $form = $formFactory->createForm();
+
+        /** @var $session \Symfony\Component\HttpFoundation\Session\Session */
+        $session = $request->getSession();
+        if (class_exists('\Symfony\Component\Security\Core\Security')) {
+            $authErrorKey = Security::AUTHENTICATION_ERROR;
+            $lastUsernameKey = Security::LAST_USERNAME;
+        } else {
+            // BC for SF < 2.6
+            $authErrorKey = SecurityContextInterface::AUTHENTICATION_ERROR;
+            $lastUsernameKey = SecurityContextInterface::LAST_USERNAME;
+        }
+        $lastUsername = (null === $session) ? '' : $session->get($lastUsernameKey);
+
+        if ($this->has('security.csrf.token_manager')) {
+            $csrfToken = $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue();
+        } else {
+            // BC for SF < 2.4
+            $csrfToken = $this->has('form.csrf_provider')
+                ? $this->get('security.csrf.token_manager')->getToken('authenticate')
+                : null;
+        }
+
         return $this->render('UserBundle:Default:index.html.twig',
             [
-                'form' => $form->createView()
+                'form' => $form->createView(),
+                'csrf_token' => $csrfToken,
+                'last_username' => $lastUsername,
             ]);
     }
 }
--- src/UserBundle/Controller/RegistrationController.php
+++ src/UserBundle/Controller/RegistrationController.php
@@ -50,7 +50,7 @@ class RegistrationController extends BaseController
         $form = $formFactory->createForm();
         $form->setData($user);
         $form->handleRequest($request);
-        $res = (string)$form->getErrors(true, true);
+        $res = $form->getErrors(true, true);
         if ($form->isValid()) {
             $event = new FormEvent($form, $request);
             $dispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
@@ -73,7 +73,14 @@ class RegistrationController extends BaseController
 
             return $response;
         } elseif ($request->isXmlHttpRequest()) {
-            return new JsonResponse($res, Response::HTTP_BAD_REQUEST);
+            // here would need bing event to extract errors (using transformer)
+            // sf has normaliser, serializer, tran
+            //sformer and other stuff related with core
+            // it uses I guess kernel binds
+            // anyway, any other problmes? not for now as I will start working on login modal now :Dokay gl
+            $errors = array_filter(explode("\n", $res->__toString()));
+            $res = ['response'=> 'failed', 'message' => $errors];
+            return new JsonResponse($res, Response::HTTP_UNAUTHORIZED);
         }
 
         return $this->render('FOSUserBundle:Registration:register.html.twig', array('form' => $form->createView()));
